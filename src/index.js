a675972290e1b2f4d4e699608d9227cd2b039014
Eit <eit@idds.dev>
2025-10-16 06:48:23 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const WEAPONS = {
+    1: "Hand-to-Hand",
+    2: "Dagger",
+    3: "Sword",
+    4: "Great Sword",
+    5: "Axe",
+    6: "Great Axe",
+    7: "Scythe",
+    8: "Polearm",
+    9: "Katana",
+    10: "Great Katana",
+    11: "Club",
+    12: "Staff",
+    25: "Archery",
+    26: "Marksmanship"
+}
